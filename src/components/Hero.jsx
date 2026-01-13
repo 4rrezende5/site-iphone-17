4582d794224d4c74d84b43c5d01d35614b4d10ac
Willians Arrezendes Leonardo <4rrezende5@gmail.com>
2026-01-12 21:55:52 -0300

    Correção no código das imagens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
       <div className='absolute top-18 bottom-0 left-0 right-0 z-0'>
         <h2>!!!!</h2>
   
-        <img src="./public/img/hero.jpg" alt="" className='w-full h-full object-cover opacity-90'/>
+        <img src="/img/hero.jpg" alt="" className='w-full h-full object-cover opacity-90'/>
 
         {/* Efeito sombreado sobre a imagem principal */}
         <div className='absolute inset-0 bg-linear-to-b  from-black/20 via-transparent to-black/90' ></div>
